34fa2cb89e53d29ee73f602c1ab52e38e85ab5cd
lucaslinyker <lucaslinyker@outlook.com>
2025-03-05 16:19:43 -0300

    agora vai 5!

---
+++ tsup.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: ['src/server.ts'],
+  outDir: 'build',
+  format: ['cjs', 'esm'],
+  dts: true,
+  clean: true,
+  ignoreWatch: ['**/*.spec.ts'],
+})
